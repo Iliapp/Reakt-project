f29bb0bd85aab17e4de6a09867a7b552e287828f
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2025-12-25 20:39:25 +0200

    do/not-all

---
+++ src/components/Todo.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+
+
+const Todo: React.FC = () => {
+
+
+
+
+    return (
+
+        <>
+            <div className="Todo-center">
+                <h1>To-do App</h1>
+                <input type={"text"}  />
+                <button onClick={}>add</button>
+
+                <ul>
+                    <li>
+
+                    </li>
+                </ul>
+
+
+            </div>
+
+
+
+        </>
+
+    );
+
+};
