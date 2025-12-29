ce3870fef3406bea0ca608e74293247635ac0453
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2025-12-29 19:06:14 +0200

    feature/add/controlled-inpud/AddTodo-function

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,8 +1,48 @@
 import React, { useState } from 'react';
+import {ITodo} from '../types.ts';
+import * as events from "node:events";
+
+
+
+
+
+
 
 
 
 const Todo: React.FC = () => {
+    const [todos, setTodos] = useState<ITodo[]>([
+        {id: 1, text: "first task", completed: false},
+        {id: 2, text: "second task", completed: true},
+        {id: 3, text: "second task", completed: false},
+
+    ]);
+
+    const [input, setInput] = useState("");
+
+    const HandleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(event.target.value);
+    };
+
+
+const AddTodo = () => {
+    if (!input.trim()) {
+        return;
+    }
+
+    const newTodo: ITodo = {
+        id: Date.now(),
+        text: input,
+        completed: false,
+    }
+
+    setTodos(prevTodos => [...prevTodos, newTodo]);
+    setInput("");
+
+
+}
+
+
 
 
 
@@ -12,14 +52,14 @@ const Todo: React.FC = () => {
         <>
             <div className="Todo-center">
                 <h1>To-do App</h1>
-                <input type={"text"}  />
-                <button onClick={}>add</button>
+                <input type={"text"}  value={input} onChange={HandleInput} />
+                <button onClick={AddTodo}>add</button>
 
-                <ul>
-                    <li>
+                {/*<ul>*/}
+                {/*    <li>*/}
 
-                    </li>
-                </ul>
+                {/*    </li>*/}
+                {/*</ul>*/}
 
 
             </div>
